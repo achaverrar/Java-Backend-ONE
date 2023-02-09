6b586f3b4b9378a74589ca0742f0f35b58ca2330
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-09 18:56:07 -0500

    añade Ejercicio 03: una reimplementación del Ejemplo 02, usando el IDE de Eclipse

---
+++ 01_Java_JRE_JDK/ejercicio_03_programando_con_eclipse/src/ejercicio_03_programando_con_eclipse/Ejemplo.java
@@ -0,0 +1,7 @@
+package ejercicio_03_programando_con_eclipse;
+
+public class Ejemplo {
+	public static void main(String[] args){
+        System.out.println("Hola mundo");
+    }
+}
---
+++ 01_Java_JRE_JDK/ejercicio_03_programando_con_eclipse/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ayxa
+ *
+ */
+module ejercicio_03_programando_con_eclipse {
+}
