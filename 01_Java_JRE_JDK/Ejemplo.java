18bd53f908d8280bcf81bcdd7bd71538ea128d04
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-08 21:58:14 -0500

    Ejericio 02-El primer programa

---
+++ 01_Java_JRE_JDK/Ejemplo.java
@@ -0,0 +1,12 @@
+// Context to wrap instructions
+// File must be named after the context
+public class Ejemplo {
+
+  // Method to execute instructions
+  public static void main(String[] args) {
+    // Printing to the console
+    System.out.println("Hola Mundo");
+  }
+}
+
+// run javac Ejemplo.java to compile Ejemplo.class
