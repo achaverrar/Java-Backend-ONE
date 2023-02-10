b507ebc4fdc1fb7924b9825f884ce13250c5aead
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-10 11:30:17 -0500

    expande Ejercicio 07: corrige errata y agrega ejemplos de bucles anidados

--- 01_Java_JRE_JDK/ejercicio_07_controlando_flujo_con_loops/src/ejercicio_07_controlando_flujo_con_loops/EjemploCiclos.java
+++ 01_Java_JRE_JDK/ejercicio_07_controlando_flujo_con_loops/src/ejercicio_07_controlando_flujo_con_loops/EjemploCiclos.java
@@ -8,34 +8,87 @@ public class EjemploCiclos {
 		// While loop
 
 		// Prints all natural numbers from 1 to 10
-		int contador = 1;        
+		int contador = 1;
 
-        while (contador <= 10) {        
-            System.out.println(contador);
-            contador++;
-        }
-        
-        // Prints out the sum of all natural numbers from 1 to 10
-        contador = 1;        
-        int total = 0;
-        while (contador <= 10) {
-        	total += contador;
-        	contador++;
-        }
-        System.out.println("La suma de todos los n naturales de 1 a 10 es: " + total);
-
-        // For loop
-        // Prints all natural numbers from 1 to 10
-        for(int i = 1; i <= 10; i++) {
-        	System.out.println(i);
-        }
-        
-        // Prints out the sum of all natural numbers from 1 to 10
-        total = 0;
-        for (int i = 1; i <= 10; i++) {
-        	total += i;
+		while (contador <= 10) {
+			System.out.println(contador);
+			contador++;
 		}
-        System.out.println("La suma de todos los n naturales de 1 a 10 es: " + total);
+
+		// Prints out the sum of all natural numbers from 1 to 10
+		contador = 1;
+		int total = 0;
+		while (contador <= 10) {
+			total += contador;
+			contador++;
+		}
+		System.out.println("La suma de todos los n naturales de 1 a 10 es: " + total);
+
+		// For loop
+		// Prints all natural numbers from 1 to 10
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+
+		// Prints out the sum of all natural numbers from 1 to 10
+		total = 0;
+		for (int i = 1; i <= 10; i++) {
+			total += i;
+		}
+		System.out.println("La suma de todos los n naturales de 1 a 10 es: " + total);
+
+		// Nested loops
+		// Prints out the multiplication tables of numbers from 1 to 10
+		for (int numTabla = 1; numTabla <= 10; numTabla++) {
+			for (int factor = 1; factor <= 10; factor++) {
+				// System.out.print() needs an argument to compile
+				System.out.print(numTabla * factor + " ");
+			}
+			// Arguments aren't mandatory for System.out.println()
+			// If it's called without arguments, it'll simply print a new line
+			System.out.println();
+		}
+
+		// Prints out a square matrix of asterisks
+		for (int fila = 0; fila < 10; fila++) {
+			for (int columna = 0; columna < 10; columna++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+		// More complex loops
+		// Prints out a upper-triangular matrix of asterisks
+		for (int fila = 0; fila < 10; fila++) {
+			for (int columna = 0; columna < 10; columna++) {
+				if (columna < fila) {
+					System.out.print("  ");
+				} else {
+					System.out.print("* ");
+				}
+			}
+			System.out.println();
+		}
+
+		// Prints out an lower-triangular matrix of asterisks
+		for (int fila = 0; fila < 10; fila++) {
+			for (int columna = 0; columna < fila; columna++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+		// Prints out a diagonal matrix of asterisks
+		for (int fila = 0; fila < 10; fila++) {
+			for (int columna = 0; columna < 10; columna++) {
+				if (columna == fila) {
+					System.out.print("* ");
+					break;
+				}
+				System.out.print("  ");
+			}
+			System.out.println();
+		}
 	}
 
 }
