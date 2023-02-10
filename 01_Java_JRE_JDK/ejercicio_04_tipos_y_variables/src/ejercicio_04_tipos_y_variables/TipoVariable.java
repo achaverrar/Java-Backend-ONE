693867f6fbc978bc203c665abccbd15c7adba724
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-09 21:45:46 -0500

    Ejercicio 04 (Clase 05: trabajando con caracteres): agrega ejemplos para ilustrar a los chars y Strings en Java

--- 01_Java_JRE_JDK/ejercicio_04_tipos_y_variables/src/ejercicio_04_tipos_y_variables/TipoVariable.java
+++ 01_Java_JRE_JDK/ejercicio_04_tipos_y_variables/src/ejercicio_04_tipos_y_variables/TipoVariable.java
@@ -20,6 +20,29 @@ public class TipoVariable {
 		byte numeroAunMasPequenio = 15;
 		float numeroDecimalPequenio = 2.5F; // lowercased F (f) in the end is also allowed
 		
+		// Chars
+		// Must use single quotes for char values
+		// Empty char ('') isn't allowed
+		// Only one character is allowed
+		/* Numbers are allowed (but are casted into the chars with the corresponding ASCII code)
+		 */ 
+		char caracter = 'a';
+		System.out.println(caracter);
+		caracter = 61;
+		System.out.println(caracter);
+		caracter = 61 + 2; // Addition is executed before casting the value
+		// If (char) is omitted, Java can't concatenate chars with numbers and cast them into chars.  
+		// If caracter + 1 is not wrapped in parenthesis, caracter is taken as a char, whereas 1 is a 
+		// numeric data type, which can't be casted into a char.
+		char nuevoCaracter = (char) (caracter + 1);
+		System.out.println(nuevoCaracter);
+		
+		// Strings
+		// In Java, they're not primitive types, but objects
+		// Must be wrapped in double quotes
+		String frase = "Alura cursos en línea";
+		System.out.println(frase);
+		
 		// Arithmetic Operators
 		// Addition/Concatenation (+) 
 		edad = 46 + 88;
