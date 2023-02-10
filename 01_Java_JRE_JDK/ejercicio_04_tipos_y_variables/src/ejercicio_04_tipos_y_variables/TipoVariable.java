09122f60b2fee9ada49241f139f7980e8bcf5907
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-09 20:59:07 -0500

    expande Ejercicio 04: agrega ejemplos de otros tipos de datos numéricos y de conversión y coerción entre tipos de datos

--- 01_Java_JRE_JDK/ejercicio_04_tipos_y_variables/src/ejercicio_04_tipos_y_variables/TipoVariable.java
+++ 01_Java_JRE_JDK/ejercicio_04_tipos_y_variables/src/ejercicio_04_tipos_y_variables/TipoVariable.java
@@ -13,6 +13,13 @@ public class TipoVariable {
 		double salario = 1250.56;
 		System.out.println(salario);
 		
+		// Other numeric data types
+		// Letters in the end of long and float values are mandatory  
+		long numeroGrande = 1222222222222222222L; // lowercased L (l) in the end is also allowed
+		short numeroPequenio = 13555;
+		byte numeroAunMasPequenio = 15;
+		float numeroDecimalPequenio = 2.5F; // lowercased F (f) in the end is also allowed
+		
 		// Arithmetic Operators
 		// Addition/Concatenation (+) 
 		edad = 46 + 88;
@@ -22,6 +29,13 @@ public class TipoVariable {
 		double tercioDouble = 1250.0 / 3;
 		int tercioInt = 1250 / 3;
 		System.out.println("Un tercio (double) de 1250 es " + tercioDouble);
-		System.out.println("Un tercio (int) de 1250 es " + tercioInt);		
+		System.out.println("Un tercio (int) de 1250 es " + tercioInt);
+		
+		// Cast data types
+		// Conversion
+		double datoDouble = 230.89;
+		int castInt = (int) datoDouble;
+		// Coercion
+		int mitadInt = 1251 / 2;
 	}
 }
