9942cbc2914556295db38a1f988ce91ed9131325
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-11 14:55:33 -0500

    reajusta los métodos de la clase Cuenta para tratar el caso en el que el valor a retirar, depositar o transferir no es positivo

--- 02_Java_OO/02_introduccion_a_la_orientacion_a_objetos/src/proyecto_bytebank/Cuenta.java
+++ 02_Java_OO/02_introduccion_a_la_orientacion_a_objetos/src/proyecto_bytebank/Cuenta.java
@@ -7,27 +7,29 @@ public class Cuenta {
 	// Attributes that are instances of other classes are set to null by default
 	// Initializing them prevents NullPointerException errors when trying to access them
 	Cliente titular = new Cliente();
-	
+
 	// Void methods don't return anything
 	// methods' names should follow lower-camel case naming convention
 	public void depositar(double valor) {
-		// The this keyword is an optional reference that helps avoid 
-		// name collisions between attributes and other kinds of variables 
-		this.saldo += valor;
+		// The this keyword is an optional reference that helps avoid
+		// name collisions between attributes and other kinds of variables
+		if (valor > 0) {
+			this.saldo += valor;
+		}
 	}
-	
-	// For methods that return something, we must declare the type 
+
+	// For methods that return something, we must declare the type
 	// of data that will be returned
 	public boolean retirar(double valor) {
-		if(this.saldo >= valor) {
+		if (valor > 0 && this.saldo >= valor) {
 			this.saldo -= valor;
 			return true;
 		}
 		return false;
 	}
-	
+
 	public boolean transferir(double valor, Cuenta cuentaDestino) {
-		if(this.saldo >= valor) {
+		if (valor > 0 && this.saldo >= valor) {
 			this.retirar(valor);
 			cuentaDestino.depositar(valor);
 			return true;
