40b71aa21a55d50efbdd97a78512fc7ccc126361
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-11 10:03:22 -0500

    agrega método depositar a la clase Cuenta

--- 02_Java_OO/02_introduccion_a_la_orientacion_a_objetos/src/proyecto_bytebank/Cuenta.java
+++ 02_Java_OO/02_introduccion_a_la_orientacion_a_objetos/src/proyecto_bytebank/Cuenta.java
@@ -5,4 +5,12 @@ public class Cuenta {
 	int agencia;
 	int numero;
 	String titular;
+	
+	// Void methods don't return anything
+	// methods' names should follow lower-camel case naming convention
+	public void depositar(double valor) {
+		// The this keyword is an optional reference that helps avoid 
+		// name collisions between attributes and other kinds of variables 
+		this.saldo += valor;
+	}
 }
