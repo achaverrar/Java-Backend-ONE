0e7b6355916f8c329c0d78a1c9bcaf6a64268a2c
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-11 11:13:34 -0500

    refactoriza código y añade comentarios

--- 02_Java_OO/02_introduccion_a_la_orientacion_a_objetos/src/proyecto_bytebank/PruebaMetodos.java
+++ 02_Java_OO/02_introduccion_a_la_orientacion_a_objetos/src/proyecto_bytebank/PruebaMetodos.java
@@ -3,18 +3,51 @@ package proyecto_bytebank;
 public class PruebaMetodos {
 
 	public static void main(String[] args) {
-		Cuenta cuenta = new Cuenta();
-		cuenta.saldo = 300;
-		cuenta.depositar(200);
-		System.out.println(cuenta.saldo);
+
+		// Testing depositar()
+		Cuenta cuentaA = new Cuenta();
+		System.out.println(cuentaA.saldo);
+		// Change attribute values directly
+		cuentaA.saldo = 300;
+		System.out.println(cuentaA.saldo);
+		// Use methods to change attribute values
+		cuentaA.depositar(200);
+		System.out.println("El nuevo saldo es: " + cuentaA.saldo);
 		
-		boolean estadoRetiro = cuenta.retirar(100);
-		System.out.println(estadoRetiro);
-		System.out.println(cuenta.saldo);
+		// Testing retirar()
+		// Case 1: success at operation
+		boolean retiroExitoso = cuentaA.retirar(100);
+		if(retiroExitoso) {
+			System.out.println("El retiro fue exitoso.");
+			System.out.println("La cuenta A tiene un saldo de " + cuentaA.saldo);
+		} else {
+			System.out.println("No se pudo efectuar el retiro");
+		}
 		
-		estadoRetiro = cuenta.retirar(600);
-		System.out.println(estadoRetiro);
-		System.out.println(cuenta.saldo);
+		// Case 2: failure at operation
+		retiroExitoso = cuentaA.retirar(600);
+		if(retiroExitoso) {
+			System.out.println("El retiro fue exitoso.");
+			System.out.println("La cuenta A tiene un saldo de " + cuentaA.saldo);
+		} else {
+			System.out.println("No se pudo efectuar el retiro");
+		}
+		
+		// Testing transferir()
+		// Setting up a new account for this test
+		Cuenta cuentaB = new Cuenta();
+		cuentaB.depositar(1000);
+		System.out.println("La cuenta B tiene un saldo de " + cuentaB.saldo);
+		
+		// Case 1: success at operation
+		boolean transferenciaExitosa = cuentaB.transferir(400, cuentaA);
+		if (transferenciaExitosa) {
+			System.out.println("La transferencia fue exitosa.");
+			System.out.println("La cuenta B tiene un saldo de " + cuentaB.saldo);
+			System.out.println("La cuenta A tiene un saldo de " + cuentaA.saldo);
+		} else {
+			System.out.println("No se pudo efectuar la transferencia.");			
+		}
 	}
 
 }
