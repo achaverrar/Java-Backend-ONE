ab11741a01b9febdd0ebc51e860a36011fba9563
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-12 10:51:48 -0500

    crea clase Funcionario y su respectiva clase de pruebas PruebaFuncionario

---
+++ 03_Java_Polimorfismo/src/bytebank_heredado/PruebaFuncionario.java
@@ -0,0 +1,13 @@
+package bytebank_heredado;
+
+public class PruebaFuncionario {
+	public static void main(String[] args) {
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNombre("Diego");
+		funcionario.setDocumento("444556665");
+		funcionario.setSalario(2000);
+		
+		System.out.println(funcionario.getSalario());
+		System.out.println(funcionario.getBonificacion());
+	}
+}
