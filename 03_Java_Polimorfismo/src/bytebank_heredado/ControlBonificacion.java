3a059e4c95b94440f1602498d8778f7703855499
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-13 11:23:02 -0500

    usa polimorfismo para refactorizar el código en clase ControlBonificacion, al unificar todas las versiones del método registrarSalario() en la más genérica (la versión de la superclase Funcionario)

--- 03_Java_Polimorfismo/src/bytebank_heredado/ControlBonificacion.java
+++ 03_Java_Polimorfismo/src/bytebank_heredado/ControlBonificacion.java
@@ -8,16 +8,4 @@ public class ControlBonificacion {
 		System.out.println("Calculo actual: " + this.suma);
 		return this.suma;
 	}
-
-	public double registrarSalario(Gerente gerente) {
-		this.suma += gerente.getBonificacion();
-		System.out.println("Calculo actual: " + this.suma);
-		return this.suma;
-	}
-	
-	public double registrarSalario(Contador contador) {
-		this.suma += contador.getBonificacion();
-		System.out.println("Calculo actual: " + this.suma);
-		return this.suma;
-	}
 }
