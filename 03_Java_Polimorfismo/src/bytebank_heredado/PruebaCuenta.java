5a8baefd6de79b9fad6de146aad73daac38f1224
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-13 15:52:39 -0500

    crea clase PruebaCuenta para evaluar las propiedades de Cuenta y sus subclases

---
+++ 03_Java_Polimorfismo/src/bytebank_heredado/PruebaCuenta.java
@@ -0,0 +1,13 @@
+package bytebank_heredado;
+
+public class PruebaCuenta {
+	public static void main(String[] args) {
+		CuentaCorriente cc = new CuentaCorriente(1, 1);
+		CuentaAhorro ca = new CuentaAhorro(2, 3);
+		
+		cc.depositar(2000);
+		cc.transferir(1000, ca);
+		System.out.println(cc.getSaldo());
+		System.out.println(ca.getSaldo());
+	}
+}
