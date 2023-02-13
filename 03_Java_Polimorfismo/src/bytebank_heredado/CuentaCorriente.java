3acef7f61d46d190a6f4c975b036e5e52551c06e
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-13 15:59:33 -0500

    sobreescribe el método retirar() de la clase Cuenta en su subclase CuentaCorriente para cobrar una comisión en cada retiro

--- 03_Java_Polimorfismo/src/bytebank_heredado/CuentaCorriente.java
+++ 03_Java_Polimorfismo/src/bytebank_heredado/CuentaCorriente.java
@@ -10,4 +10,15 @@ public class CuentaCorriente extends Cuenta {
 		// super() accesses the superclass' constructor
 		super(agencia, numero);
 	}
+	
+	// @Override allows subclasses overriding methods existing in their
+	// corresponding superclass.
+	// If this annotation is skipped, there won't be errors if the original
+	// method's signature is changed and so, there's a chance the new method
+	// doesn't override that of the superclass 
+	@Override
+	public boolean retirar(double valor) {
+		double comision = 0.2;
+		return super.retirar(valor + comision);
+	}
 }
