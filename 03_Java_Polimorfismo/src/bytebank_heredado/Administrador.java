42b708abc01c73404b54956611b7c4d94b90b398
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-14 11:45:07 -0500

    replica el campo clave, el constructor y los métodos setClave e iniciarSesion de la clase Gerente en la clase Administrador para darle la funcionalidad de autenticarse en el sistema

--- 03_Java_Polimorfismo/src/bytebank_heredado/Administrador.java
+++ 03_Java_Polimorfismo/src/bytebank_heredado/Administrador.java
@@ -1,6 +1,19 @@
 package bytebank_heredado;
 
 public class Administrador extends Funcionario {
+	private String clave;
+
+	public Administrador() {
+		this.clave = "AluraCursosOnLine";
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	public boolean iniciarSesion(String clave) {
+		return this.clave == clave;
+	}
 
 	@Override
 	public double getBonificacion() {
