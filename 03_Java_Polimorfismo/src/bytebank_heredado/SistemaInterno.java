396e85d4f3ee336caf89a717ecadfd8b031513eb
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-14 11:42:34 -0500

    crea el método autenticar en la clase SistemaInterno para informar sobre el resultado del método iniciarSesion de la clase Gerente

--- 03_Java_Polimorfismo/src/bytebank_heredado/SistemaInterno.java
+++ 03_Java_Polimorfismo/src/bytebank_heredado/SistemaInterno.java
@@ -1,5 +1,17 @@
 package bytebank_heredado;
 
 public class SistemaInterno {
+	private String clave = "12345";
 
+	public boolean autenticar(Gerente gerente) {
+		boolean puedeIniciarSesion = gerente.iniciarSesion(clave);
+
+		if (puedeIniciarSesion) {
+			System.out.println("Login exitoso");
+			return true;
+		} else {
+			System.out.println("Error en login");
+			return false;
+		}
+	}
 }
