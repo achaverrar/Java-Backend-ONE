bc4f3bae18698d3cf1c6ce6bd46eff8be9c76124
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-15 16:28:04 -0500

    agrega métodos a la clase Flujo para explorar la pila de ejecución en Java, usando la perspectiva debug

--- 04_Java_Excepciones/src/java_pila_ejecucion/Flujo.java
+++ 04_Java_Excepciones/src/java_pila_ejecucion/Flujo.java
@@ -1,4 +1,23 @@
 package java_pila_ejecucion;
 
 public class Flujo {
+	public static void main(String args[]) {
+		System.out.println("Inicio main");
+		metodo1();
+		System.out.println("Fin main");
+	}
+
+	private static void metodo1() {
+		System.out.println("Inicio metodo1");
+		metodo2();
+		System.out.println("Fin metodo1");
+	}
+
+	private static void metodo2() {
+		System.out.println("Inicio metodo2");
+		for (int i = 0; i <= 5; i++) {
+			System.out.println(i);
+		}
+		System.out.println("Fin metodo2");
+	}
 }
