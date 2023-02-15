9308c6c4ae035b02311cfbd0d99d2f014d1e9485
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-15 16:53:46 -0500

    utiliza un multi-catch en la clase Flujo para manejar las dos excepciones creadas anteriormente

--- 04_Java_Excepciones/src/java_pila_ejecucion/Flujo.java
+++ 04_Java_Excepciones/src/java_pila_ejecucion/Flujo.java
@@ -21,23 +21,29 @@ public class Flujo {
 			// An uncaught exception will stop the execution of the program
 			// try and catch statements go hand in hand
 			try {
-				/* int num = 0;
-				int resultado = i / num;
-				System.out.println(resultado);
-				*/
+				// When the execution finds an exception,
+				// skips the rest of the body of the try statement
+				// and jumps to the body of the catch statement
+				if (i == 3) {
+					int num = 0;
+					int resultado = i / num;
+					System.out.println(resultado);
+				}
+				// When i is 3, there's only one exception being caught
+				// and that's an Arithmetic exception
+				// The code never gets to that of the type NullPointer
 				String test = null;
 				System.out.println(test.toString());
 			}
 			// Exceptions are objects in Java
 			// There are multiple types of exceptions
-			// Case 1: catching an exception of the type ArithmeticException
-			/*catch (ArithmeticException exception) {
+			// The multi-catch statements in Java allow us to catch
+			// multiple types of exceptions in the same catch
+			// Case 3: handling two types of exception in the same catch
+			catch (ArithmeticException | NullPointerException exception) {
+				System.out.println("Atrapa Excepcion");
 				System.out.println(exception.getMessage());
 				exception.printStackTrace();
-			}*/ 
-			catch (NullPointerException exception) {
-				System.out.println(exception.getMessage());
-				exception.printStackTrace();				
 			}
 		}
 		System.out.println("Fin metodo2");
