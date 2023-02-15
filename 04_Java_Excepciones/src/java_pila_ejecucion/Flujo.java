2e71d62ad7b159ba01708a7e965b3f603f47b3e2
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-15 18:04:00 -0500

    reemplaza las excepciones anteriores por una creada y lanzada manualmente

--- 04_Java_Excepciones/src/java_pila_ejecucion/Flujo.java
+++ 04_Java_Excepciones/src/java_pila_ejecucion/Flujo.java
@@ -15,37 +15,11 @@ public class Flujo {
 
 	private static void metodo2() {
 		System.out.println("Inicio metodo2");
-		for (int i = 0; i <= 5; i++) {
-			System.out.println(i);
-			// try and catch statements help us handle exceptions
-			// An uncaught exception will stop the execution of the program
-			// try and catch statements go hand in hand
-			try {
-				// When the execution finds an exception,
-				// skips the rest of the body of the try statement
-				// and jumps to the body of the catch statement
-				if (i == 3) {
-					int num = 0;
-					int resultado = i / num;
-					System.out.println(resultado);
-				}
-				// When i is 3, there's only one exception being caught
-				// and that's an Arithmetic exception
-				// The code never gets to that of the type NullPointer
-				String test = null;
-				System.out.println(test.toString());
-			}
-			// Exceptions are objects in Java
-			// There are multiple types of exceptions
-			// The multi-catch statements in Java allow us to catch
-			// multiple types of exceptions in the same catch
-			// Case 3: handling two types of exception in the same catch
-			catch (ArithmeticException | NullPointerException exception) {
-				System.out.println("Atrapa Excepcion");
-				System.out.println(exception.getMessage());
-				exception.printStackTrace();
-			}
-		}
-		System.out.println("Fin metodo2");
+		// We can create and throw our own exceptions in Java
+		// It's best practice to throw an exception without creating a reference to it
+		// To show a customized message, we pass it as a String to its constructor 
+		throw new ArithmeticException("Mensaje personalizado");
+		// Only instances of the type Exception can thrown 
+		// No lines of code in this body are allowed after the exception was explicitly thrown
 	}
 }
