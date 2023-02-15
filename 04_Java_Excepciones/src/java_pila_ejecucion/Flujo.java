b4da34e617270898da91bdeaffd53444c82054f0
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-15 16:39:37 -0500

    introduce una excepción aritmética en la clase Flujo y usa una declaración try/catch para manejarla

--- 04_Java_Excepciones/src/java_pila_ejecucion/Flujo.java
+++ 04_Java_Excepciones/src/java_pila_ejecucion/Flujo.java
@@ -17,6 +17,21 @@ public class Flujo {
 		System.out.println("Inicio metodo2");
 		for (int i = 0; i <= 5; i++) {
 			System.out.println(i);
+			// try and catch statements help us handle exceptions
+			// An uncaught exception will stop the execution of the program
+			// try and catch statements go hand in hand
+			try {
+				int num = 0;
+				int resultado = i / num;
+				System.out.println(resultado);
+			}
+			// Exceptions are objects in Java
+			// There are multiple types of exceptions
+			// Case 1: catching an exception of the type ArithmeticException
+			catch (ArithmeticException exception) {
+				System.out.println(exception.getMessage());
+				exception.printStackTrace();
+			}
 		}
 		System.out.println("Fin metodo2");
 	}
