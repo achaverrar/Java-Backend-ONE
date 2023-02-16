aed7fd7184350092fcf28dd1c2fafd46aaf76b13
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-16 12:21:11 -0500

    usa la palabra reservada finally para refactorizar el código en el manejo de la excepción en la clase PruebaConexion

--- 04_Java_Excepciones/src/java_pila_ejecucion/PruebaConexion.java
+++ 04_Java_Excepciones/src/java_pila_ejecucion/PruebaConexion.java
@@ -5,11 +5,23 @@ public class PruebaConexion {
 		Conexion con = new Conexion();
 		try {
 			con.leerDatos();
-			con.cerrar();
 		} catch (IllegalStateException ex) {
 			System.out.println("Recibiendo excepcion");
 			ex.printStackTrace();
-			con.cerrar();
+		} 
+		// The keyword finally is optional as long as 
+		// there's a catch block in the try-catch statement
+		//
+		// finally doesn't handle exceptions and its body
+		// is executed regardless of the exception being 
+		// caught or not.
+		//
+		// There can only be a single finally block per
+		// try-catch statement and it must be the last
+		// block in the statement
+		finally {
+			System.out.println("Ejecutando finally");
+			con.cerrar();			
 		}
 	}
 }
