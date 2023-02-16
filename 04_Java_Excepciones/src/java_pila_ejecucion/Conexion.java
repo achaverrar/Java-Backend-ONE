c36ab92ac09b32fc39643d39c9b43ab538a7a14a
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-16 12:12:57 -0500

    en el proyecto/paquete java_pila_ejecucion crea la clase Conexion

---
+++ 04_Java_Excepciones/src/java_pila_ejecucion/Conexion.java
@@ -0,0 +1,16 @@
+package java_pila_ejecucion;
+
+public class Conexion {
+	public Conexion() {
+		System.out.println("Abriendo conexion");
+	}
+
+	public void leerDatos() {
+		System.out.println("Recibiendo datos");
+		// throw new IllegalStateException();
+	}
+
+	public void cerrar() {
+		System.out.println("Cerrando conexion");
+	}
+}
