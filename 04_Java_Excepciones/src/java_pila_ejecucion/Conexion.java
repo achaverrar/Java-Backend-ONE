d2c6fcb20fa99c418de164df94732b3a650a32d5
Ayxa Chaverra <achaverrar@gmail.com>
2023-02-17 11:14:03 -0500

    hace que la excepción en la clase Conexion ahora la lance el constructor y no el método leerDatos(), y adapta el try-catch de la clase PruebaConexion para manejar la excepción según el nuevo cambio

--- 04_Java_Excepciones/src/java_pila_ejecucion/Conexion.java
+++ 04_Java_Excepciones/src/java_pila_ejecucion/Conexion.java
@@ -3,11 +3,11 @@ package java_pila_ejecucion;
 public class Conexion {
 	public Conexion() {
 		System.out.println("Abriendo conexion");
+		throw new IllegalStateException();
 	}
 
 	public void leerDatos() {
 		System.out.println("Recibiendo datos");
-		throw new IllegalStateException();
 	}
 
 	public void cerrar() {
--- 04_Java_Excepciones/src/java_pila_ejecucion/PruebaConexion.java
+++ 04_Java_Excepciones/src/java_pila_ejecucion/PruebaConexion.java
@@ -2,26 +2,24 @@ package java_pila_ejecucion;
 
 public class PruebaConexion {
 	public static void main(String[] args) {
-		Conexion con = new Conexion();
+		// Initializing an object as null and wrapping its
+		// constructor in a try block is common when there's
+		// a chance an Exception is thrown while initializing
+		// the object. This way, we can handle the exception
+		Conexion con = null;
 		try {
+			con = new Conexion();
 			con.leerDatos();
 		} catch (IllegalStateException ex) {
 			System.out.println("Recibiendo excepcion");
 			ex.printStackTrace();
-		} 
-		// The keyword finally is optional as long as 
-		// there's a catch block in the try-catch statement
-		//
-		// finally doesn't handle exceptions and its body
-		// is executed regardless of the exception being 
-		// caught or not.
-		//
-		// There can only be a single finally block per
-		// try-catch statement and it must be the last
-		// block in the statement
-		finally {
+		} finally {
 			System.out.println("Ejecutando finally");
-			con.cerrar();			
+			// This condition prevents getting a NullPointerException
+			// in case Conexion() never gets to finish execution
+			if (con != null) {
+				con.cerrar();
+			}
 		}
 	}
 }
