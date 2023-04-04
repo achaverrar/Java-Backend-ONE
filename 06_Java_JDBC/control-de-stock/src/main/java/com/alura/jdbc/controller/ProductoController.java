f1b1d5197a8767b3ee75d00d9201a331a4fa3cea
Ayxa Chaverra <achaverrar@gmail.com>
2023-04-04 10:50:51 -0500

    use the try-catch with resources structure to close connectionsacross all the code in ProductoController.java

--- 06_Java_JDBC/control-de-stock/src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ 06_Java_JDBC/control-de-stock/src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -16,98 +16,107 @@ public class ProductoController {
 
 	public int modificar(String nombre, String descripcion, Integer id, Integer cantidad) throws SQLException {
 		ConnectionFactory factory = new ConnectionFactory();
-		Connection con = factory.recuperaConexion();
+		final Connection con = factory.recuperaConexion();
 
-		PreparedStatement statement = con.prepareStatement(
-				"UPDATE producto SET " + "nombre = ?, " + "descripcion = ?, " + "cantidad = ? " + "WHERE id = ?");
-		statement.setString(1, nombre);
-		statement.setString(2, descripcion);
-		statement.setInt(3, cantidad);
-		statement.setInt(4, id);
+		try (con) {
+			final PreparedStatement statement = con.prepareStatement(
+					"UPDATE producto SET " + "nombre = ?, " + "descripcion = ?, " + "cantidad = ? " + "WHERE id = ?");
+			
+			try (statement) {
+				statement.setString(1, nombre);
+				statement.setString(2, descripcion);
+				statement.setInt(3, cantidad);
+				statement.setInt(4, id);
 
-		statement.execute();
+				statement.execute();
 
-		con.close();
-
-		return statement.getUpdateCount();
+				return statement.getUpdateCount();
+			}
+		}
 	}
 
 	public int eliminar(Integer id) throws SQLException {
 		ConnectionFactory factory = new ConnectionFactory();
-		Connection con = factory.recuperaConexion();
+		final Connection con = factory.recuperaConexion();
 
-		PreparedStatement statement = con.prepareStatement("DELETE FROM producto WHERE id = ?");
+		try (con) {
+			final PreparedStatement statement = con.prepareStatement("DELETE FROM producto WHERE id = ?");
 
-		statement.setInt(1, Integer.valueOf(id));
+			try (statement) {
+				statement.setInt(1, Integer.valueOf(id));
+				statement.execute();
 
-		statement.execute();
-
-		con.close();
-
-		return statement.getUpdateCount();
+				return statement.getUpdateCount();
+			}
+		}
 	}
 
 	public List<Map<String, String>> listar() throws SQLException {
 		ConnectionFactory factory = new ConnectionFactory();
-		Connection con = factory.recuperaConexion();
+		final Connection con = factory.recuperaConexion();
 
-		PreparedStatement statement = con.prepareStatement("SELECT id, nombre, descripcion, cantidad FROM producto");
+		try (con) {
+			final PreparedStatement statement = con
+					.prepareStatement("SELECT id, nombre, descripcion, cantidad FROM producto");
+			
+			try (statement) {
+				statement.execute();
 
-		statement.execute();
+				ResultSet resultSet = statement.getResultSet();
 
-		ResultSet resultSet = statement.getResultSet();
+				List<Map<String, String>> resultado = new ArrayList<>();
 
-		List<Map<String, String>> resultado = new ArrayList<>();
+				while (resultSet.next()) {
+					Map<String, String> fila = new HashMap<>();
+					fila.put("id", String.valueOf(resultSet.getInt("id")));
+					fila.put("nombre", resultSet.getString("nombre"));
+					fila.put("descripcion", resultSet.getString("descripcion"));
+					fila.put("cantidad", String.valueOf(resultSet.getInt("cantidad")));
+					resultado.add(fila);
+				}
+				return resultado;
+			}
 
-		while (resultSet.next()) {
-			Map<String, String> fila = new HashMap<>();
-			fila.put("id", String.valueOf(resultSet.getInt("id")));
-			fila.put("nombre", resultSet.getString("nombre"));
-			fila.put("descripcion", resultSet.getString("descripcion"));
-			fila.put("cantidad", String.valueOf(resultSet.getInt("cantidad")));
-			resultado.add(fila);
 		}
-
-		con.close();
-
-		return resultado;
 	}
 
 	public void guardar(Map<String, String> producto) throws SQLException {
-		ConnectionFactory factory = new ConnectionFactory();
-		Connection con = factory.recuperaConexion();
-		/* If there's an error with the transaction and it can't be completed,
-		 * all progress made is undone. 
-		*/
-		con.setAutoCommit(false);
-		
-		PreparedStatement statement = con.prepareStatement(
-				"INSERT INTO producto " + "(nombre, descripcion, cantidad) " + "VALUES (?, ?, ?)",
-				Statement.RETURN_GENERATED_KEYS);
-
 		String nombre = producto.get("nombre");
 		String descripcion = producto.get("descripcion");
 		Integer cantidad = Integer.valueOf(producto.get("cantidad"));
 		Integer maximaCantidad = 50;
 
-		try {
-			do {
-				int cantidadParaGuardar = Math.min(cantidad, maximaCantidad);
-				ejecutarRegistro(statement, nombre, descripcion, cantidadParaGuardar);
-				cantidad -= maximaCantidad;
-			} while (cantidad > 0);
-			
-			/* 
-			 * When auto-commit is set to false, we have to explicitly 
-			 * commit the changes to the database
+		ConnectionFactory factory = new ConnectionFactory();
+		final Connection con = factory.recuperaConexion();
+
+		try (con) {
+			/*
+			 * If there's an error with the transaction and it can't be completed, all
+			 * progress made is undone.
 			 */
-			con.commit();			
-		} catch (Exception e) {
-			con.rollback();
+			con.setAutoCommit(false);
+
+			final PreparedStatement statement = con.prepareStatement(
+					"INSERT INTO producto " + "(nombre, descripcion, cantidad) " + "VALUES (?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+
+			try (statement) {
+				do {
+					int cantidadParaGuardar = Math.min(cantidad, maximaCantidad);
+					ejecutarRegistro(statement, nombre, descripcion, cantidadParaGuardar);
+					cantidad -= maximaCantidad;
+				} while (cantidad > 0);
+
+				/*
+				 * When auto-commit is set to false, we have to explicitly commit the changes to
+				 * the database
+				 */
+				con.commit();
+			} catch (Exception e) {
+				con.rollback();
+			}
+
 		}
-		
-		statement.close();
-		con.close();
 	}
 
 	private void ejecutarRegistro(PreparedStatement statement, String nombre, String descripcion, Integer cantidad)
@@ -118,10 +127,16 @@ public class ProductoController {
 
 		statement.execute();
 
-		ResultSet resultSet = statement.getGeneratedKeys();
+		/*
+		 * Java 9's syntax to close connections automatically. It only works with
+		 * entities that implement the interface Autoclosable.
+		 */
+		final ResultSet resultSet = statement.getGeneratedKeys();
 
-		while (resultSet.next()) {
-			System.out.println(String.format("Fue insertado el producto de id %d", resultSet.getInt(1)));
+		try (resultSet) {
+			while (resultSet.next()) {
+				System.out.println(String.format("Fue insertado el producto de id %d", resultSet.getInt(1)));
+			}
 		}
 	}
 
