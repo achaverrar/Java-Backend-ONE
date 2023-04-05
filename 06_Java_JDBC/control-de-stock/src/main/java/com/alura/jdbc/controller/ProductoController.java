c14e6f9c15a35c187ca023f503aaf625038847a2
Ayxa Chaverra <achaverrar@gmail.com>
2023-04-05 11:39:23 -0500

    create the prductoDAO private field in the class ProductoController and initialize it in the class' constructor

--- 06_Java_JDBC/control-de-stock/src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ 06_Java_JDBC/control-de-stock/src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,6 +14,11 @@ import com.alura.jdbc.factory.ConnectionFactory;
 import com.alura.jdbc.modelo.Producto;
 
 public class ProductoController {
+	private ProductoDAO productoDAO;
+
+	public ProductoController() {
+		this.productoDAO = new ProductoDAO(new ConnectionFactory().recuperaConexion());
+	}
 	
 	public int modificar(String nombre, String descripcion, Integer id, Integer cantidad) throws SQLException {
 		ConnectionFactory factory = new ConnectionFactory();
