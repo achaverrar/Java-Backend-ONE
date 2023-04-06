30a2e96ea65937de26337580b96677fe1e1b6c01
Ayxa Chaverra <achaverrar@gmail.com>
2023-04-06 09:56:31 -0500

    add the categoriaId field and its corresponding setter and getter methods in the Producto class, and change the toString method to also include the categoriaId value

--- 06_Java_JDBC/control-de-stock/src/main/java/com/alura/jdbc/modelo/Producto.java
+++ 06_Java_JDBC/control-de-stock/src/main/java/com/alura/jdbc/modelo/Producto.java
@@ -6,6 +6,7 @@ public class Producto {
 	private String nombre;
 	private String descripcion;
 	private Integer cantidad;
+	private Integer categoriaId;
 
 	public Producto(String nombre, String descripcion, Integer cantidadInt) {
 		this.nombre = nombre;
@@ -19,47 +20,44 @@ public class Producto {
 		this.descripcion = descripcion;
 		this.cantidad = cantidadInt;
 	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getDescripcion() {
-		return descripcion;
-	}
-
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-
-	public Integer getCantidad() {
-		return cantidad;
-	}
-
-	public void setCantidad(Integer cantidad) {
-		this.cantidad = cantidad;
-	}
-
-	public void setId(int id) {
+	
+	public void setId(Integer id) {
 		this.id = id;
 	}
 	
 	public Integer getId() {
 		return this.id;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+	
+	public void setCategoriaId(Integer categoriaId) {
+		this.categoriaId = categoriaId;
+	}
+	
+	public Integer getCategoriaId() {
+		return this.categoriaId;
+	}
 	
 	@Override
 	public String toString() {
 		return String.format("{"
-				+ "id: %s, nombre: %s, descripcion: %s, cantidad: %d}",
+				+ "id: %s, nombre: %s, descripcion: %s, cantidad: %d, categoria_id: %s}",
 				this.id,
 				this.nombre,
 				this.descripcion,
-				this.cantidad);
+				this.cantidad,
+				this.categoriaId);
 	}
 
 }
