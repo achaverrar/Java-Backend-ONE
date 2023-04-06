1cb4662806329b84777152376976e2e9f240652d
Ayxa Chaverra <achaverrar@gmail.com>
2023-04-06 13:26:32 -0500

    add the listarConProductos method to the CategoriaDAO class to generate a list of categories, each of them with a list of related products

--- 06_Java_JDBC/control-de-stock/src/main/java/com/alura/jdbc/dao/CategoriaDAO.java
+++ 06_Java_JDBC/control-de-stock/src/main/java/com/alura/jdbc/dao/CategoriaDAO.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.alura.jdbc.modelo.Categoria;
+import com.alura.jdbc.modelo.Producto;
 
 public class CategoriaDAO {
 	private Connection con;
@@ -27,6 +28,49 @@ public class CategoriaDAO {
 					while (resultSet.next()) {
 						var categoria = new Categoria(resultSet.getInt("id"), resultSet.getString("nombre"));
 						resultado.add(categoria);
+					}
+				}
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+		return resultado;
+	}
+	
+	public List<Categoria> listarConProductos() {
+		List<Categoria> resultado = new ArrayList<>();
+
+		try {
+			var querySelect = "SELECT c.id, c.nombre, p.id, p.nombre, p.cantidad "
+					+ "FROM categoria c INNER JOIN producto p "
+					+ "ON c.id = p.categoria_id";
+			
+			final PreparedStatement statement = con.prepareStatement(querySelect);
+			
+			try (statement) {
+				final ResultSet resultSet = statement.executeQuery();
+				try (resultSet) {
+					while (resultSet.next()) {
+						int categoriaId = resultSet.getInt("id");
+						String categoriaNombre = resultSet.getString("nombre");
+						
+						var categoria = resultado
+								.stream()
+								.filter(cat -> cat.getId().equals(categoriaId))
+								.findAny().orElseGet(() -> {
+									Categoria cat = new Categoria(categoriaId, categoriaNombre);
+									
+									resultado.add(cat);
+									
+									return cat;
+								});
+						Producto producto = new Producto(
+								resultSet.getInt("p.id"),
+								resultSet.getString("p.nombre"),
+								resultSet.getInt("p.cantidad"));
+						
+						categoria.agregar(producto);
 					}
 				}
 			}
