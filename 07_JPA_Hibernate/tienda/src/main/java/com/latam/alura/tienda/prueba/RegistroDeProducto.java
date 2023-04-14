a7c6b661399493cd9926c7cee6a2f0db4612db6f
Ayxa Chaverra <achaverrar@gmail.com>
2023-04-13 22:42:10 -0500

    07 Persistencia con Java - Hibernate: create the ProductoDao and JPAUtils classes to refactor the code in the RegistroDeProducto class

--- 07_JPA_Hibernate/tienda/src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
+++ 07_JPA_Hibernate/tienda/src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
@@ -7,6 +7,8 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import com.latam.alura.tienda.modelo.Producto;
+import com.latam.alura.tienda.utils.JPAUtils;
+import com.latam.alura.tienda.dao.ProductoDao;
 
 public class RegistroDeProducto {
 
@@ -16,11 +18,14 @@ public class RegistroDeProducto {
 		celular.setDescripcion("teléfono usado");
 		celular.setPrecio(new BigDecimal("1000"));
 		
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("tienda");
-		EntityManager em = factory.createEntityManager();
+		EntityManager em = JPAUtils.getEntityManager();
+		
+		ProductoDao productoDao = new ProductoDao(em);
 		
 		em.getTransaction().begin();
-		em.persist(celular);
+
+		productoDao.guardar(celular);
+		
 		em.getTransaction().commit();
 		em.close();
 	}
