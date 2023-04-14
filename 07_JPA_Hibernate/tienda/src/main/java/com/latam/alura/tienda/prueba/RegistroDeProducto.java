c9066bf24afba9cc935a4814e89efcbb7061cf15
Ayxa Chaverra <achaverrar@gmail.com>
2023-04-14 12:03:17 -0500

    07 Persistencia con Java - Hibernate: remove the code that tested the entities' lifecycle and uncomment the commented code, refactor the code and use the ProductoDao new method to print the descriptions of all products to the console

--- 07_JPA_Hibernate/tienda/src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
+++ 07_JPA_Hibernate/tienda/src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
@@ -1,6 +1,7 @@
 package com.latam.alura.tienda.prueba;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -15,36 +16,32 @@ import com.latam.alura.tienda.dao.ProductoDao;
 public class RegistroDeProducto {
 
 	public static void main(String[] args) {
+		registrarProducto();
+		EntityManager em = JPAUtils.getEntityManager();
+		ProductoDao productoDao = new ProductoDao(em);
+		Producto producto = productoDao.consultaPorId(1l);
+		System.out.println(producto.getNombre());
+		
+		List<Producto> productos = productoDao.consultarTodos();
+		productos.forEach(prod -> System.out.println(prod.getDescripcion()));
+		
+	}
+
+	private static void registrarProducto() {
 		Categoria celulares = new Categoria("CELULARES");
-		//Producto celular = new Producto("Samsung", "teléfono usado", new BigDecimal("1000"), celulares);
+		Producto celular = new Producto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
 		
 		EntityManager em = JPAUtils.getEntityManager();
-		
-		//ProductoDao productoDao = new ProductoDao(em);
-		//CategoriaDao categoriaDao = new CategoriaDao(em);
+		ProductoDao productoDao = new ProductoDao(em);
+		CategoriaDao categoriaDao = new CategoriaDao(em);
 		
 		em.getTransaction().begin();
 
-		//categoriaDao.guardar(celulares);
-		//productoDao.guardar(celular);
+		categoriaDao.guardar(celulares);
+		productoDao.guardar(celular);
 		
-		em.persist(celulares);
-		celulares.setNombre("LIBROS");
-		
-		em.flush();
-		em.clear();
-		
-		celulares = em.merge(celulares);
-		celulares.setNombre("SOFTWARES");
-		
-		//em.getTransaction().commit();
-		em.flush();
-		em.clear();
-		
-		celulares = em.merge(celulares);
-		em.remove(celulares);
-		em.flush();
-
+		em.getTransaction().commit();
+		em.close();
 	}
 
 }
