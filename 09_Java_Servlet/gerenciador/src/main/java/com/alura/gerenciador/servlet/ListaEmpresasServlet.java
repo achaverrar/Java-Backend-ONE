c8b3c9ee0042b78673415e2308612abdc03020bf
Ayxa Chaverra <achaverrar@gmail.com>
2023-04-16 21:21:46 -0500

    09 - Java Servlet: implement client side redirection after submitting the form

--- 09_Java_Servlet/gerenciador/src/main/java/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
+++ 09_Java_Servlet/gerenciador/src/main/java/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class ListaEmpresasServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		DB baseDeDatos = new DB();
 		List<Empresa> listaEmpresas = baseDeDatos.getEmpresas();
--- 09_Java_Servlet/gerenciador/src/main/java/com/alura/gerenciador/servlet/NuevaEmpresaServlet.java
+++ 09_Java_Servlet/gerenciador/src/main/java/com/alura/gerenciador/servlet/NuevaEmpresaServlet.java
@@ -16,35 +16,41 @@ public class NuevaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/*
-	 * the service and doPost methods receive the same parameters
-	 * service() allows get requests as well as post ones
-	 * doPost() only allows post requests (returning 405 errors to not allowed request)
+	 * the service and doPost methods receive the same parameters service() allows
+	 * get requests as well as post ones doPost() only allows post requests
+	 * (returning 405 errors to not allowed request)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("Nueva empresa registrada");
-		
+
 		String nombreEmpresa = request.getParameter("nombre");
 		String paramFechaApertura = request.getParameter("fecha");
 		Date fechaApertura = null;
-		
+
 		try {
 			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy");
 			fechaApertura = sdf.parse(paramFechaApertura);
 		} catch (ParseException e) {
 			throw new ServletException(e);
 		}
-		
+
 		Empresa empresa = new Empresa();
 		empresa.setNombre(nombreEmpresa);
 		empresa.setFechaApertura(fechaApertura);
-		
+
 		DB baseDeDatos = new DB();
 		baseDeDatos.agregarEmpresa(empresa);
-		
+
+		// The code below causes client side redirection
+		// Do not use a backslash at the beginning of the string
+		response.sendRedirect("listaEmpresas");
+
+		// The code below causes server side redirection
 		// Call JSP file
-		RequestDispatcher rd = request.getRequestDispatcher("/nuevaEmpresaRegistrada.jsp");
-		request.setAttribute("empresa", empresa.getNombre());
-		rd.forward(request, response);
+//		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas");
+//		request.setAttribute("empresa", empresa.getNombre());
+//		rd.forward(request, response);
 	}
 
 }
