8fceb58186d06b52c909aed92c98a8bfffb4d6c6
Ayxa Chaverra <achaverrar@gmail.com>
2023-04-18 16:02:28 -0500

    10 - Java Servlet: create the UnicaEntradaServlet servlet to handle all the requests to the server and test that it's properly connected

---
+++ 10_Java_Servlet_MVC/gerenciador/src/main/java/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -0,0 +1,27 @@
+package com.alura.gerenciador.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/entrada")
+public class UnicaEntradaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String param = request.getParameter("accion");
+		
+		if(param.equals("listaEmpresas")) {
+			System.out.println("Listando empresas");
+		} else if (param.equals("MostrarEmpresa")) {
+			System.out.println("Mostrando una empresa");
+		} else if (param.equals("EliminarEmpresa")) {
+			System.out.println("Eliminando empresas");
+		}
+	}
+
+}
