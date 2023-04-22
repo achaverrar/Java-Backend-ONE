19df104caa02a7f3485116575f834750a764d0fa
Ayxa Chaverra <achaverrar@gmail.com>
2023-04-22 15:09:57 -0500

    12 - Spring Boot 3: use methods from the ResponseEntity class in the MedicoController class to standardize the response code to delete requests

--- 12_Spring_Boot_3_API_Rest_Buenas_Practicas/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ 12_Spring_Boot_3_API_Rest_Buenas_Practicas/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -38,9 +39,10 @@ public class MedicoController {
     // Logical deletions
     @DeleteMapping("/{id}")
     @Transactional
-    public void eliminarMedico(@PathVariable Long id) {
+    public ResponseEntity eliminarMedico(@PathVariable Long id) {
         Medico medico = medicoRepository.getReferenceById(id);
         medico.desactivarMedico();
+        return ResponseEntity.noContent().build();
     }
 
 }
