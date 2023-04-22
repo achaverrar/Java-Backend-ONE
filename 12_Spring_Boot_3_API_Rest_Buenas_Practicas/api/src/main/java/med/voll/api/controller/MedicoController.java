c57f8df178124cb69111380bf611008ccde3fffb
Ayxa Chaverra <achaverrar@gmail.com>
2023-04-22 15:01:46 -0500

    12 - Spring Boot 3: create starter files for the course project

---
+++ 12_Spring_Boot_3_API_Rest_Buenas_Practicas/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,46 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.medico.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @PostMapping
+    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico) {
+        medicoRepository.save(new Medico(datosRegistroMedico));
+    }
+
+    // @PageableDefault overwrites default settings
+    // Query params overwrite our settings
+    @GetMapping
+    public Page<DatosListadoMedico> listadoMedicos(@PageableDefault(size=1) Pageable paginacion) {
+        return medicoRepository.findByActivoTrue(paginacion).map(DatosListadoMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void actualizarMedico(@RequestBody @Valid DatosActualizarMedico datosActualizarMedico) {
+        Medico medico = medicoRepository.getReferenceById(datosActualizarMedico.id());
+        medico.actualizarDatos(datosActualizarMedico);
+    }
+
+    // Logical deletions
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void eliminarMedico(@PathVariable Long id) {
+        Medico medico = medicoRepository.getReferenceById(id);
+        medico.desactivarMedico();
+    }
+
+}
